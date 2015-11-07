e9c02a67c958f6a6d8e85a8aafca1220ab9bbc64
mikelangston <mikemac8203@gmail.com>
2015-11-07 15:58:40 -0500

    added dependent destroy to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ActiveRecord::Base
   belongs_to :user
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_one :location
+  has_one :location, dependent: :destroy
   accepts_nested_attributes_for :location
   mount_uploader :featured_image, FeaturedImageUploader
 
