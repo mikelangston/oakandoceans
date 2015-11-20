263726ccf60df01daffcca0161bee037872833ef
mikelangston <mikemac8203@gmail.com>
2015-11-20 18:27:37 -0500

    updated group view and added city_state method for locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,8 @@ class Location < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true
   validates :postal_code, presence: true
+
+  def city_state
+    "#{self.city}, #{self.state}"
+  end
 end
