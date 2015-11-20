fb5cae17ec2504c72ce35e37c8b6a99748639d7c
mikelangston <mikemac8203@gmail.com>
2015-11-20 18:32:51 -0500

    added rescue for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |e|
+    redirect_to root_url, alert: e.message 
+  end
+
   protected
 
   def after_sign_in_path_for(resource)
