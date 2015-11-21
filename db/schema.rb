48ba2b64f141b1cd7095fefd0e14812bab927e9e
mikelangston <mikemac8203@gmail.com>
2015-11-20 19:37:06 -0500

    added descriptions for events

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119133850) do
+ActiveRecord::Schema.define(version: 20151121003602) do
 
   create_table "events", force: :cascade do |t|
-    t.integer  "group_id",   limit: 4
-    t.string   "name",       limit: 255
-    t.integer  "venue_id",   limit: 4
+    t.integer  "group_id",    limit: 4
+    t.string   "name",        limit: 255
+    t.integer  "venue_id",    limit: 4
     t.date     "date"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.text     "description", limit: 65535
   end
 
   create_table "groups", force: :cascade do |t|
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,13 +2,14 @@
 #
 # Table name: events
 #
-#  id         :integer          not null, primary key
-#  group_id   :integer
-#  name       :string(255)
-#  venue_id   :integer
-#  date       :date
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  group_id    :integer
+#  name        :string(255)
+#  venue_id    :integer
+#  date        :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :text(65535)
 #
 
 require 'rails_helper'
